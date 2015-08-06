221452101908108f504f8aa1522016835f72e608
Chris Kolody <chriskolody@Chriss-Mac-mini.local>
2015-08-06 14:47:30 -0600

    Initial Commit

---
+++ Algorythm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Algorythm
+//
+//  Created by Chris Kolody on 8/6/15.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
